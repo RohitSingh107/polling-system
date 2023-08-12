d89f320527ab2f52876c5f98f09378860bc37215
Rohit <RohitSinghEmail@protonmail.com>
2023-08-12 19:36:26 +0530

    everything containerized working

--- polls/urls.py
+++ polls/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('<int:poll_id>/', views.detail, name='detail'),
     path('<int:poll_id>/results/', views.results, name='results'),
     path('<int:poll_id>/vote/', views.vote, name='vote'),
+    path('create/', views.create_poll, name='create'),
+    path('<int:poll_id>/add_choice/', views.add_choice, name='add_choice'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Choice, Poll
+from .forms import PollForm, ChoiceForm
 
 def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    latest_poll_list = Poll.objects.order_by('-pub_date')
     context = {'latest_poll_list': latest_poll_list}
     return render(request, 'polls/index.html', context)
 
@@ -29,3 +30,30 @@ def vote(request, poll_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(poll.id,)))
+
+
+
+def create_poll(request):
+    if request.method == 'POST':
+        form = PollForm(request.POST)
+        if form.is_valid():
+            poll = form.save()
+            # return redirect('polls:index')  # Redirect to poll list
+            return redirect('polls:detail', poll_id=poll.id)  # Redirect to poll detail
+    else:
+        form = PollForm()
+    return render(request, 'polls/create_poll.html', {'form': form})
+
+
+def add_choice(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    if request.method == 'POST':
+        form = ChoiceForm(request.POST)
+        if form.is_valid():
+            choice = form.save(commit=False)
+            choice.poll = poll
+            choice.save()
+            return redirect('polls:detail', poll_id=poll.id)  # Redirect to poll detail
+    else:
+        form = ChoiceForm()
+    return render(request, 'polls/add_choice.html', {'form': form, 'poll': poll})
